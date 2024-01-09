4244320455544f37004a988c2f1c231c8fd10d36
FeryaelJustice <nano9gs@hotmail.es>
2024-01-09 19:53:42 +0100

    second commit

---
+++ pages/api/shortenURL.js
@@ -0,0 +1,5 @@
+export default async function handler(req, res) {
+  const { url } = req.body;
+  const shortURL = Math.random().toString(36).substr(2, 5);
+  res.status(200).send({ url, shortURL })
+}
